e1416ba9cd41f6b40f8ed0251b2135e065115cb4
Peiqiang Hao(Tinyfool) <tinyfool@gmail.com>
2013-07-12 23:49:46 +0800

    create iOS libary project

---
+++ lasem-ios/lasem-ios/lasem_ios.h
@@ -0,0 +1,13 @@
+//
+//  lasem_ios.h
+//  lasem-ios
+//
+//  Created by pei hao on 13-7-12.
+//  Copyright (c) 2013年 pei hao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface lasem_ios : NSObject
+
+@end
---
+++ lasem-ios/lasem-ios/lasem_ios.m
@@ -0,0 +1,13 @@
+//
+//  lasem_ios.m
+//  lasem-ios
+//
+//  Created by pei hao on 13-7-12.
+//  Copyright (c) 2013年 pei hao. All rights reserved.
+//
+
+#import "lasem_ios.h"
+
+@implementation lasem_ios
+
+@end
